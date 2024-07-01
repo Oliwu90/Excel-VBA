900661a5922a08a8a9a9ed5d50f995d7539acdb7
Oliver Wu <gablewithbird@gmail.com>
2024-07-01 23:57:47 +0800

    Create SpeedOff.vb

---
+++ SpeedOff.vb
@@ -0,0 +1,8 @@
+Sub Speed_Turn_On()
+With Application
+            .DisplayAlerts = False
+            .ScreenUpdating = False
+            .DisplayStatusBar = False
+            .EnableEvents = False
+End With
+End Sub
